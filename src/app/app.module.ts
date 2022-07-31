53e279ca54e0fdd65bee62852dda7bbe4337adb8
Carlos Nava <44716371+coldplayer2@users.noreply.github.com>
2022-07-31 14:20:12 -0300

    Implementa backend a login frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,11 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
 
 
 @NgModule({
@@ -37,9 +39,12 @@ import { LoginComponent } from './components/login/login.component';
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ 
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
